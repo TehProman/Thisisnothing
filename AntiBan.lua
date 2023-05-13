e20630b187bcea8f7f5a48ba1f73fbe030a5d8c3
Proman <66687123+TehProman@users.noreply.github.com>
2023-05-13 11:26:40 +0800

    Update and rename AntiBan.lua to AntiSlashKill.lua

--- AntiBan.lua
+++
@@ -1,9 +0,0 @@
---Anti Screenshot and Backtrace
-print("initiating Anti Screenshot and Backtrace...")
-setfflag("AbuseReportScreenshotPercentage", 0)
-setfflag("DFFlagAbuseReportScreenshot", "False")
-setfflag("DFStringCrashPadUploadToBacktraceToBacktraceBaseUrl", "")
-setfflag("DFIntCrashUploadToBacktracePercentage", "0")
-setfflag("DFStringCrashUploadToBacktraceBlackholeToken", "")
-setfflag("DFStringCrashUploadToBacktraceWindowsPlayerToken", "")
-print("Done! XD")
